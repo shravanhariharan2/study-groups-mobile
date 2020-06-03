7e0187b821c309fe2d2ea9f28cd7a999e435252b
shravanhariharan2 <smhariha@ucsd.edu>
2020-06-02 18:48:27 -0700

    Fix groups initialization

--- backend/src/main/java/linecooks/backend/repositories/GroupJoinedRepository.java
+++ backend/src/main/java/linecooks/backend/repositories/GroupJoinedRepository.java
@@ -2,9 +2,12 @@ package linecooks.backend.repositories;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.bson.types.ObjectId;
 
-public interface GroupJoinedRepository {
+import linecooks.backend.models.GroupJoined;
+
+public interface GroupJoinedRepository extends MongoRepository<GroupJoined, String>{
 
     List<ObjectId> findByuserId(ObjectId userId);
     List<ObjectId> findBygroupId(ObjectId groupId);
